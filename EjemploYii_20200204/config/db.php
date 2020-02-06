c7c8d2306e7defb53fdf583aac5b6f4fe6ddc97b
Oscar Megía López <pfgytk@gmail.com>
2020-02-06 13:15:56 +0100

    Proyecto inicial

---
+++ EjemploYii_20200204/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ejemployii_20200204',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
